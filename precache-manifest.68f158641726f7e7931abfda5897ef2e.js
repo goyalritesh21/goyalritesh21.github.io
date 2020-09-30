c5f24aadd9867cf77b9c01f3da4ad0263141d8bd
Ritesh Goyal <goyalritesh21@gmail.com>
2020-09-30 19:57:32 +0200

    Updates

---
+++ precache-manifest.68f158641726f7e7931abfda5897ef2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf9f101db3eaf64224172dbe6ee1788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aaaacd59470844065a1",
+    "url": "/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "8fa05a725ce0020bc0c0",
+    "url": "/static/js/2.4a91bcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a91bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaaacd59470844065a1",
+    "url": "/static/js/main.6697254f.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
